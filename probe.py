f9c8ffd0d2d62dfa88f5553f0aa12cd947eefb7a
matthewhatch <mhatch@squadlocker.com>
2018-12-02 15:18:24 -0500

    format output

--- probe.py
+++ probe.py
@@ -3,4 +3,4 @@ from wifi import Cell, Scheme
 networks = list(Cell.all('wlan0'))
 
 for net in networks:
-    print('%s \t %s'.format(x,x*x))%(net.ssid, net.quality)
+    print('%s \t %s'.format(net,net*net))%(net.ssid, net.quality)
