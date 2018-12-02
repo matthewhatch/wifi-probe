0a71af9f24bbd452908af0fa99103745574484f5
matthewhatch <mhatch@squadlocker.com>
2018-12-02 18:36:34 -0500

    add table output

--- probe.py
+++ probe.py
@@ -1,7 +1,30 @@
 from wifi import Cell, Scheme
+import texttable as tt
+
+# set up the table
+tab = tt.Texttable()
+ssidList = list()
+qualityList = list()
+frequencyList = list()
+signalList = list()
+modeList = list()
+addressList = list()
+typeList = list()
+
+headings = ['ssid','quality','frequency','signal','mode','address','type'];
+tab.header(headings)
 
 networks = list(Cell.all('wlan0'))
-
 for net in networks:
     enc_type = net.encryption_type if net.encrypted else 'open'
-    print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, enc_type)
+    # print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, enc_type)
+    ssidList.append(net.ssid)
+    qualityList.append(net.quality)
+    frequencyList.append(net.frequency)
+    signalList.append(net.signal)
+    modeList.append(net.mode)
+    addressList.append(net.address)
+    typeList.append(enc_type)
+
+wifi_table = tab.draw()
+print(wifi_table)
