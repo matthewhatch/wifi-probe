1f9ad5451553801bb73bb9d63fd4cf5cc4824bed
matthewhatch <mhatch@squadlocker.com>
2018-12-02 17:00:30 -0500

    show encryption_type or open

--- probe.py
+++ probe.py
@@ -3,4 +3,5 @@ from wifi import Cell, Scheme
 networks = list(Cell.all('wlan0'))
 
 for net in networks:
-    print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, net.encrypted, net.encryption_type)
+    enc_type = net.encryption_type if net.encrypted else 'open'
+    print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, enc_type)
