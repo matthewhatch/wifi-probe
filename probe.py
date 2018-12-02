99a9c020432f860cd8b9e39088b675a9bc022511
matthewhatch <mhatch@squadlocker.com>
2018-12-02 18:51:06 -0500

    loop

--- probe.py
+++ probe.py
@@ -1,5 +1,6 @@
 from wifi import Cell, Scheme
 import texttable as tt
+import os
 
 # set up the table
 tab = tt.Texttable()
@@ -14,21 +15,23 @@ typeList = list()
 headings = ['ssid','quality','frequency','signal','mode','address','type'];
 tab.header(headings)
 
-networks = list(Cell.all('wlan0'))
-for net in networks:
-    enc_type = net.encryption_type if net.encrypted else 'open'
-    # print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, enc_type)
-    ssidList.append(net.ssid)
-    qualityList.append(net.quality)
-    frequencyList.append(net.frequency)
-    signalList.append(net.signal)
-    modeList.append(net.mode)
-    addressList.append(net.address)
-    typeList.append(enc_type)
+while(True:
+    os.system('clear')
+    networks = list(Cell.all('wlan0'))
+    for net in networks:
+        enc_type = net.encryption_type if net.encrypted else 'open'
+        # print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, enc_type)
+        ssidList.append(net.ssid)
+        qualityList.append(net.quality)
+        frequencyList.append(net.frequency)
+        signalList.append(net.signal)
+        modeList.append(net.mode)
+        addressList.append(net.address)
+        typeList.append(enc_type)
 
-for row in zip(ssidList, qualityList, frequencyList, signalList, modeList, addressList, typeList):
-    tab.add_row(row)
+    for row in zip(ssidList, qualityList, frequencyList, signalList, modeList, addressList, typeList):
+        tab.add_row(row)
 
 
-wifi_table = tab.draw()
-print(wifi_table)
+    wifi_table = tab.draw()
+    print(wifi_table)
