6ee1b14e3090d279792f6b3b2daa78c1b315e328
matthewhatch <mhatch@squadlocker.com>
2018-12-02 16:56:46 -0500

    show encryption_type

--- probe.py
+++ probe.py
@@ -3,4 +3,4 @@ from wifi import Cell, Scheme
 networks = list(Cell.all('wlan0'))
 
 for net in networks:
-    print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, net.encrypted)
+    print(net.ssid, net.quality, net.frequency, net.signal, net.mode, net.address, net.encrypted, net.encryption_type)
