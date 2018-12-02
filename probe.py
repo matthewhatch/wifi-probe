a945edc21792ce95af495644b9912abbc560c7c7
matthewhatch <mhatch@squadlocker.com>
2018-12-02 14:58:24 -0500

    format output

--- probe.py
+++ probe.py
@@ -1,7 +1,6 @@
 from wifi import Cell, Scheme
 
 networks = list(Cell.all('wlan0'))
-col_width = max(len(word) for row in networks for word in row) + 2
 
 for net in networks:
     print('%s \t %s'%(net.ssid, net.quality)).format(x,x*x)
