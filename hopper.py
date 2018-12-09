112c02f3dcc75ce536dd19107c1b219d66c8289a
matthewhatch <mhatch@squadlocker.com>
2018-12-09 10:34:31 -0500

    add lcd display

--- hopper.py
+++ hopper.py
@@ -1,5 +1,20 @@
 import threading, os, time, random
+import Adafruit_CharLCD as LCD
 from scapy.all import *
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 25  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en        = 24
+lcd_d4        = 23
+lcd_d5        = 17
+lcd_d6        = 18
+lcd_d7        = 22
+lcd_backlight = 4
+
+lcd_columns = 16
+lcd_rows    = 2
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
 
 def hopper(iface):
     n = 1
@@ -17,9 +32,12 @@ def findSSID(pkt):
        if pkt.getlayer(Dot11).addr2 not in F_bssids:
            F_bssids.append(pkt.getlayer(Dot11).addr2)
            ssid = pkt.getlayer(Dot11Elt).info
+           display_ssid = str(ssid).strip('b').strip("'")
            if ssid == '' or pkt.getlayer(Dot11Elt).ID != 0:
                print("Hidden Network Detected")
-           print("Network Detected: %s - %d networks found" % (ssid, len(F_bssids)))
+           print("Network Detected: %s - %d networks found" % (display_ssid, len(F_bssids)))
+           lcd.clear()
+           lcd.message("SSID: %s\nTotal: %d" % (display_ssid, len(F_bssids)))
 
 if __name__ == "__main__":
     interface = "wlan1mon"
