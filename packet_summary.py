a5f6a8815ee5f8bc3d56504ec45c362545b0130c
matthewhatch <mhatch@squadlocker.com>
2018-12-10 09:08:15 -0500

    packet summary test

--- packet_summary.py
+++ packet_summary.py
@@ -1,15 +1,40 @@
 from scapy.all import *
 
-counter = 0
+def insert_ap(pkt):
+    ## Done in the lfilter param
+    # if Dot11Beacon not in pkt and Dot11ProbeResp not in pkt:
+    #     return
+    bssid = pkt[Dot11].addr3
+    if bssid in aps:
+        return
+    p = pkt[Dot11Elt]
+    cap = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}"
+                      "{Dot11ProbeResp:%Dot11ProbeResp.cap%}").split('+')
+    ssid, channel = None, None
+    crypto = set()
+    while isinstance(p, Dot11Elt):
+        if p.ID == 0:
+            ssid = p.info
+        elif p.ID == 3:
+            channel = ord(p.info)
+        elif p.ID == 48:
+            crypto.add("WPA2")
+        elif p.ID == 221: #and p.info.startswith('\x00P\xf2\x01\x01\x00'):
+            crypto.add("WPA")
+        p = p.payload
+    if not crypto:
+        if 'privacy' in cap:
+            crypto.add("WEP")
+        else:
+            crypto.add("OPN")
+    print("NEW AP: %r [%s], channed %d, %s" % (ssid, bssid, channel,' / '.join(crypto)))
+    aps[bssid] = (ssid, channel, crypto)
 
-def custom_action(packet):
-    global counter
-    counter += 1
-    # print('couting some shite')
-    if packet.haslayer(UDP):
-        print(packet.getlayer(UDP).src)
+aps = {}
+sniff(iface='wlan1mon', prn=insert_ap, store=False,
+      lfilter=lambda p: (Dot11Beacon in p or Dot11ProbeResp in p))
     # print(packet[0][1].dst)
     # return "Packet $s: %s ==> %s" % (counter, packet[0][1].src, packet[0][1].dst)
 
 ## Setup sniff, filtering for IP traffic
-sniff(filter="ip", prn=custom_action)
+# sniff(filter="ip", prn=custom_action)
